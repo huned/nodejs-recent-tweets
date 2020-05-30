08c712c2044c824bf92a1495d9fc70a1ee198808
Huned Botee <24231+huned@users.noreply.github.com>
2020-05-29 21:26:40 -0700

    Add tests.

--- test/test_index.js
+++ test/test_index.js
@@ -1,9 +1,27 @@
 const assert = require('assert')
+const getRecentTweets = require('../')
 
 // NOTE: don't use arrow functions with mocha.
 // See https://github.com/mochajs/mocha/issues/2018
-describe('Index', function () {
-  it('true === true (Replace this test with more useful ones)', function () {
-    assert.strictEqual(true, true)
+describe('getRecentTweets', function () {
+  it('rejects without username', async function () {
+    assert.rejects(getRecentTweets(), { message: /twitter username/ })
+  })
+
+  it('parses tweets into JSON', async function () {
+    // Allow enough time for chromium to load, etc
+    this.timeout(10000)
+
+    const mockTwitterURLFn = () => `file://${process.cwd()}/test/mocks/twitter_nodeknockout_202005292057.html`
+    const tweets = await getRecentTweets('nodeknockout', null, mockTwitterURLFn)
+    const tweet = tweets[0]
+
+    assert.ok(Array.isArray(tweets))
+    assert.strictEqual(tweets.length, 20)
+
+    assert.strictEqual(tweet.t, '2020-05-25 00:00:00')
+    assert.ok(/Finished my 5th @nodeknockout/.test(tweet.text))
+    assert.strictEqual(tweet.hrefs[0], 'https://www.youtube.com/watch?v=JkUukMBfD4E&')
+    assert.strictEqual(tweet.hrefs[1], 'https://www.nodeknockout.com/entries/53-team-tyson')
   })
 })
