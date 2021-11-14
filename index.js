87ba023c42bddaf6696bb6976becfd79d68c86af
Huned Botee <huned@hunedbotee.com>
2021-11-14 10:39:48 -0800

    Make this work a little better.
    - Notably, don't keep a list of tweet DOM elements because those seem to get
    destroyed. Instead, just extract the tweet data as we encounter each tweet
    DOM element.
    - Cleaned up debugging output. (Less chatty because chatty was slow.)
    - Other tidying up

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ if (require.main === module) {
   debug(`getting recent tweets for ${twitterUsername}`)
   getRecentTweets(twitterUsername)
     .then(tweets => {
-      debug(`got tweets: ${JSON.stringify(tweets, null, 2)}`)
       process.stdout.write(JSON.stringify(tweets))
       process.stdout.write('\n')
     })
