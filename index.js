1bea83c50bfb0f315496aad870dcddfb262b9126
Huned Botee <huned@hunedbotee.com>
2021-11-15 14:31:42 -0800

    Some tidying
    - show usage message if no username given
    - exit with status=-2 on errors

--- index.js
+++ index.js
@@ -5,43 +5,58 @@ const getTweets = require('./lib/recent_tweets')
 const debug = require('debug')('recent-tweets')
 const readline = require('readline')
 
-const main = async options => {
-  if (!options.username) {
-    throw new Error('Provide a username')
-    // TODO show help message and set process.exitCode
-  }
-
-  debug(`getting recent tweets for ${options.username}`)
-  const tweets = await getTweets(options.username, {
-    expandShortlinks: options['expand-shortlinks'],
-    deviceDescriptor: options.device
-  })
-
-  process.stdout.write(JSON.stringify(tweets))
+const showUsage = () => {
+  const message = 'twls [options...] twitter_username'
+  process.stdout.write(message)
   process.stdout.write('\n')
 }
 
+const main = async options => {
+  try {
+    debug(`getting recent tweets for ${options.username}`)
+    const tweets = await getTweets(options.username, {
+      expandShortlinks: options['expand-shortlinks'],
+      deviceDescriptor: options.device
+    })
+
+    process.stdout.write(JSON.stringify(tweets))
+    process.stdout.write('\n')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 if (require.main === module) {
-  const options = commandLineArgs([
-    { name: 'username', alias: 'u', type: String, defaultOption: true },
-    { name: 'expand-shortlinks', alias: 'x', type: Boolean, defaultValue: false },
-    { name: 'device', alias: 'd', type: String, defaultValue: 'iPhone 11 Pro Max' }
-  ])
+  let options
 
-  debug(`options: ${JSON.stringify(options, null, 2)}`)
+  try {
+    options = commandLineArgs([
+      { name: 'username', alias: 'u', type: String, defaultOption: true },
+      { name: 'expand-shortlinks', alias: 'x', type: Boolean, defaultValue: false },
+      { name: 'device', alias: 'd', type: String, defaultValue: 'iPhone 11 Pro Max' }
+    ])
 
-  if (!options.username) {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    })
+    debug(`options: ${JSON.stringify(options, null, 2)}`)
+  } catch (error) {
+    options = undefined
+    process.exitCode = -2
+    showUsage()
+  }
 
-    rl.on('line', async username => {
-      options.username = username
+  if (options) {
+    if (!options.username) {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      })
+
+      rl.on('line', async username => {
+        options.username = username
+        main(options)
+      })
+    } else {
       main(options)
-    })
-  } else {
-    main(options)
+    }
   }
 } else {
   debug('required as a module')
