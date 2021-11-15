f9c5d9b19bb452138522a811423f014f844cc48b
Huned Botee <huned@hunedbotee.com>
2021-11-15 12:31:28 -0800

    Get arguments from command line, not environment.

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const getRecentTweets = require('./lib/recent_tweets')
+#!/usr/bin/env node
+
+const commandLineArgs = require('command-line-args')
+const getTweets = require('./lib/recent_tweets')
 const debug = require('debug')('recent-tweets')
 
 if (require.main === module) {
-  const twitterUsername = process.env.TWITTER_USERNAME
-  debug(`getting recent tweets for ${twitterUsername}`)
-  getRecentTweets(twitterUsername)
-    .then(tweets => {
-      process.stdout.write(JSON.stringify(tweets))
-      process.stdout.write('\n')
-    })
+  const options = commandLineArgs([
+    { name: 'username', alias: 'u', type: String, defaultOption: true },
+    { name: 'expand-shortlinks', alias: 'x', type: Boolean, defaultValue: false },
+    { name: 'device', alias: 'd', type: String, defaultValue: 'iPhone 11 Pro Max' }
+  ])
+
+  if (!options.username) {
+    throw new Error('Provide a username')
+    // TODO show help message and set process.exitCode
+  }
+
+  debug(`options: ${JSON.stringify(options, null, 2)}`)
+
+  debug(`getting recent tweets for ${options.username}`)
+  getTweets(options.username, {
+    expandShortlinks: options['expand-shortlinks'],
+    deviceDescriptor: options.device
+  }).then(tweets => {
+    process.stdout.write(JSON.stringify(tweets))
+    process.stdout.write('\n')
+  })
 } else {
   debug('required as a module')
 }
 
-module.exports = getRecentTweets
+module.exports = getTweets
