2a0364be2f1e67747fbbe1c21b99b4480581694a
Huned Botee <huned@hunedbotee.com>
2021-11-14 21:47:33 -0800

    Add tests and some clean-up.
    Notable changes:
    
    - exports `getTweets` now (vs the previous `getRecentTweets`)
    - `getTweets(username, options)` now accepts an (optional) options object
    - shortlinks are not expanded by default (too many network requests --> slow)
    - adds a permalink to the tweet json

--- test/test_index.js
+++ test/test_index.js
@@ -1,33 +1,46 @@
 const assert = require('assert')
-const fs = require('fs')
-const getRecentTweets = require('../')
+const path = require('path')
+const getTweets = require('../')
 
 // NOTE: don't use arrow functions with mocha.
 // See https://github.com/mochajs/mocha/issues/2018
-describe('getRecentTweets', function () {
-  it('rejects without username', async function () {
-    assert.rejects(getRecentTweets(), { message: /twitter username/ })
-  })
+describe('getTweets', function () {
+  describe('returns well formed tweets', function () {
+    it('@huned', async function () {
+      const mockFile = path.join(__dirname, './data/huned-2021-11-15T04:55:05.933Z.html')
+      const tweets = await getTweets('huned', { _mockURL: `file:${mockFile}` })
 
-  it('parses tweets into JSON', async function () {
-    assert.fail('Need to reimplement for new twitter markup')
+      assert.ok(Array.isArray(tweets))
+      assert.strictEqual(tweets.length, 9)
 
-    // Allow enough time for chromium to load, etc
-    this.timeout(30000)
+      const tweet = tweets[0]
+      assert.strictEqual(tweet.time, '2015-03-29T04:26:37.000Z')
+      assert.strictEqual(tweet.textContent, 'Ignorance is our greatest enemy.')
+      assert.strictEqual(tweet.permalink, 'https://twitter.com/huned/status/582036113142910976')
+      assert.deepStrictEqual(tweet.links, [])
+    })
 
-    const mockGetHTMLFn = () => {
-      const mockFile = `${process.cwd()}/test/mocks/twitter_nodeknockout_202005292057.html`
-      return fs.readFileSync(mockFile).toString()
-    }
-    const tweets = await getRecentTweets('nodeknockout', mockGetHTMLFn)
-    const tweet = tweets[0]
+    it('@newsyc250', async function () {
+      const mockFile = path.join(__dirname, './data/newsyc250-2021-11-15T05:37:30.546Z.html')
+      const tweets = await getTweets('newsyc250', { _mockURL: `file:${mockFile}` })
 
-    assert.ok(Array.isArray(tweets))
-    assert.strictEqual(tweets.length, 20)
+      assert.ok(Array.isArray(tweets))
+      assert.strictEqual(tweets.length, 20)
 
-    assert.strictEqual(tweet.t, '2019-11-25 00:00:00')
-    assert.ok(/Finished my 5th @nodeknockout/.test(tweet.text))
-    assert.strictEqual(tweet.hrefs[0], 'https://www.youtube.com/watch?v=JkUukMBfD4E&')
-    assert.strictEqual(tweet.hrefs[1], 'https://www.nodeknockout.com/entries/53-team-tyson')
+      const tweet = tweets[0]
+      assert.strictEqual(tweet.time, '2021-11-08T17:48:19.000Z')
+      assert.strictEqual(tweet.textContent, 'The benefits of staying off social media https://durmonski.com/life-advice/benefits-of-staying-off-social-media/… (http://news.ycombinator.com/item?id=29149961…)')
+      assert.strictEqual(tweet.permalink, 'https://twitter.com/newsyc250/status/1457766951720259585')
+      assert.deepStrictEqual(tweet.links, [
+        {
+          href: 'https://t.co/keyrMBLqLX?amp=1',
+          textContent: 'https://durmonski.com/life-advice/benefits-of-staying-off-social-media/…'
+        },
+        {
+          href: 'https://t.co/fnN9jbawcu?amp=1',
+          textContent: 'http://news.ycombinator.com/item?id=29149961…'
+        }
+      ])
+    })
   })
 })
